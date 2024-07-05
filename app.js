6dccdfa1702b3cd8d50410059bf576daa36265ba
Jose V <alvajose35@gmail.com>
2024-07-04 22:11:49 -0400

    Basic Authentication

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const url = 'mongodb://localhost:27017/nucampsite';
 const connect = mongoose.connect(url, {});
 
 connect.then(() => console.log('Connected correctly to server'),
-  err => console.log(err)
+	err => console.log(err)
 );
 
 var app = express();
@@ -31,6 +31,35 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+// Authenticate user
+function auth(req, res, next) {
+	console.log(req.headers);
+	const authHeader = req.headers.authorization;
+	if (!authHeader) {
+		const err = new Error('You are not authenticated!');
+		res.setHeader('WWW-Authenticate', 'Basic');
+		err.status = 401;
+		return next(err);
+	}
+	
+	const auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+	const user = auth[0];
+	const pass = auth[1];
+
+	if (user === 'admin' && pass === 'password') {
+		return next(); //authorized
+	}
+	else {
+		const err = new Error('You are not authenticated!');
+		res.setHeader('WWW-Authenticate', 'Basic');
+		err.status = 401;
+		return next(err);	
+	}
+}
+
+app.use(auth);
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
@@ -41,18 +70,18 @@ app.use('/partners', partnerRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+	next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+	// render the error page
+	res.status(err.status || 500);
+	res.render('error');
 });
 
 module.exports = app;
