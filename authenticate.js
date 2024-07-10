ca3471a80e8aa252e976e957547f12cd13c667d0
Jose V <alvajose35@gmail.com>
2024-07-10 04:22:02 -0400

    Workshop Week 3

--- authenticate.js
+++ authenticate.js
@@ -44,3 +44,14 @@ exports.jwtPassport = passport.use(
 );
 
 exports.verifyUser = passport.authenticate('jwt', {session: false});
+
+exports.verifyAdmin = (req, res, next) => {
+    if (req.user.admin) {
+        return next();
+    }
+    else {
+        const err = new Error('You are not authorized to perform this operation! NOT AN ADMIN');
+        res.statusCode = 403;
+        return next(err)
+    }
+}
