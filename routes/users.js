c0ba38f9b5b4abae631216c3504dbf9e45ca057d
Jose V <alvajose35@gmail.com>
2024-07-09 22:25:51 -0400

    Mongoose Population

--- routes/users.js
+++ routes/users.js
@@ -15,18 +15,35 @@ router.post('/signup', (req, res) => {
 	User.register(
 		new User({username: req.body.username}),
 		req.body.password,
-		err => {
+		(err, user) => {
 			if (err) {
 				res.statusCode = 500;
 				res.setHeader('Content-Type', 'application/json');
                 res.json({err: err});
 			}
 			else {
-				passport.authenticate('local')(req, res, () =>  {
-					res.statusCode = 200;
-					res.setHeader('Content-Type', 'application/json');
-					res.json({success: true, status: 'Registration Successful!'});
-				});
+				// Check to see if first and last name were sent
+				if (req.body.firstname) {
+                    user.firstname = req.body.firstname;
+                }
+                if (req.body.lastname) {
+                    user.lastname = req.body.lastname;
+                }
+				user.save()
+				.then(
+					passport.authenticate('local')(req, res, () =>  {
+						res.statusCode = 200;
+						res.setHeader('Content-Type', 'application/json');
+						res.json({success: true, status: 'Registration Successful!'});
+					})
+				)
+				.catch(err => {
+                        res.statusCode = 500;
+                        res.setHeader('Content-Type', 'application/json');
+                        res.json({err: err});
+                        return;
+                    }
+				);
 			}
 		}
 	);
